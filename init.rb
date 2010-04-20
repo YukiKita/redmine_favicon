9f4b0d0706e72e7493bda41713f5f24601511579
Yuki Kita <kita.yuki@gmail.com>
2010-04-20 23:57:55 +0900

    Changed path

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 class MenuManagerHooks < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context = { })
-    '<link rel="shortcut icon" href="/plugin_assets/redmine_logo/images/favicon.ico" />'
+    '<link rel="shortcut icon" href="/plugin_assets/redmine_favicon/images/favicon.ico" />'
   end
 end
