9e1e2f3a63971dd030c85a40993a05de283b073c
Yuki Kita <kita.yuki@gmail.com>
2010-04-21 20:11:34 +0900

    Changed favicon for IE, updated README, changed class name

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
-class MenuManagerHooks < Redmine::Hook::ViewListener
+class FaviconHooks < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context = { })
-    '<link rel="shortcut icon" href="/plugin_assets/redmine_favicon/images/favicon.ico" />'
+    '<link rel="shortcut icon" href="/plugin_assets/redmine_favicon/images/favicon.ico">'
   end
 end
