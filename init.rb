a59cd60c79cf4e3d1e12f56e53201d1abc2fd06d
Yuki Kita <kita.yuki@gmail.com>
2010-05-31 23:01:03 +0900

    added support for sub-uri

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 class FaviconHooks < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context = { })
-    '<link rel="shortcut icon" href="/plugin_assets/redmine_favicon/images/favicon.ico">'
+    "<link rel=\"shortcut icon\" href=\"#{Redmine::Utils::relative_url_root}/plugin_assets/redmine_favicon/images/favicon.ico\">"
   end
 end
